6f69ac145976700ff332e1c39d81d2e579116aa5
juanraba <jrabanillog@gmail.com>
2018-07-19 12:02:55 -0400

    add ejer4.2

--- ejer4.rb
+++ ejer4.rb
@@ -5,8 +5,14 @@ class Dog
     @raza = hash[:raza]
     @color = hash[:color]
   end
+
+  def ladrar
+    puts "#{@nombre} está ladrando."
+  end
 end
 
 propiedades = { nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café' }
 
 dog = Dog.new(propiedades)
+
+dog.ladrar
